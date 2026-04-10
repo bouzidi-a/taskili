2d703a21ff46acd5bdba87c84d0cdd1dcd9ff91d
Abdou BOUZIDI <abdobouzidi64@gmail.com>
2026-04-10 01:42:50 +0100

    not important changes

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CLUSTER, MONGO_APP_NAME } = process.env;
 
 const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/?appName=${MONGO_APP_NAME}`;
-
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
